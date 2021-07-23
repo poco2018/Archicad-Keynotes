8796a44022ddc3d2675cd630b6fa0c7d8a6c9586
poco2018 <37485109+poco2018@users.noreply.github.com>
2021-07-23 01:14:55 -0400

    Add files via upload

---
+++ Github - second release/Source/update_07_22_21/AdditionalJSONCommands.hpp
@@ -0,0 +1,35 @@
+#if !defined (ADDITIONALJSONCOMMANDS_HPP)
+#define ADDITIONALJSONCOMMANDS_HPP
+
+#pragma once
+
+#include	"APIEnvir.h"
+#include	"ACAPinc.h"
+
+class AdditionalJSONCommand : public API_AddOnCommand {
+public:
+	virtual GS::String							GetNamespace() const override;
+	virtual GS::Optional<GS::UniString>			GetSchemaDefinitions() const override;
+	virtual GS::Optional<GS::UniString>			GetInputParametersSchema() const override;
+	virtual GS::Optional<GS::UniString>			GetResponseSchema() const override;
+	virtual API_AddOnCommandExecutionPolicy		GetExecutionPolicy() const override { return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; }
+	virtual void								OnResponseValidationFailed(const GS::ObjectState& response) const override;
+};
+
+
+class KeyNoteCommand : public AdditionalJSONCommand {
+public:
+
+	virtual GS::String							GetName() const override;
+
+
+	virtual GS::Optional<GS::UniString>			GetInputParametersSchema() const override;
+	
+
+	virtual GS::ObjectState						Execute(const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
+
+	
+};
+
+
+#endif
